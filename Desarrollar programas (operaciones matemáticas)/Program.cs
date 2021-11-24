9f548894fc9a5684e07fc1a9edac9128190a4d1c
abner <abner@DESKTOP-KC8BSDT>
2021-11-24 09:49:56 -0400

    Add project files.

---
+++ Desarrollar programas (operaciones matemáticas)/Program.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desarrollar_programas__operaciones_matemáticas_
+{
+    class Program
+    {
+        private static double vRadio;
+
+        static void Main(string[] args)
+        {
+            #region 1)Área de cuadrado;
+            //Int32 vL;
+            //double vA;
+            // Console.WriteLine("Area de Cuadrado");
+            //Console.Write("Favor ingrese el lado del cuadrado: ");
+            //vL = Convert.ToInt32(Console.ReadLine());
+
+            //vA = vL * vL;
+            //Console.Write("El AREA DEL CUADRADO ES: {0}", vA);
+            //Console.ReadKey();
+            #endregion
+
+
+
+            #region 2)Área de Rectangulo;
+            //Int32 vb,vh;
+            //    double vA;
+            //Console.WriteLine("Area de Rectangulo");
+            //Console.Write("Favor ingrese la base: ");
+            //vb = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Favor ingrese la altura: ");
+            //vh = Convert.ToInt32(Console.ReadLine());
+            //vA = vb * vh;
+            //Console.Write("El AREA DEL RECTANGULO ES: {0}", vA);
+            //Console.ReadKey();
+            #endregion
+
+            #region 3)Área de tiangulo;
+            //Int32 vb, vh;
+            //double vA;
+            //Console.WriteLine("Area de Triangulo");
+            //Console.Write("Favor ingrese la base (b): ");
+            //vb = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Favor ingrese la altura (h): ");
+            //vh = Convert.ToInt32(Console.ReadLine());
+            //vA = (vb * vh);
+            //Console.Write("El AREA DEL TRIANGULO ES: {0}", vA);
+            //Console.ReadKey();
+            #endregion
+
+            #region 4)Área de trapecio;
+
+            //int a, b, h;
+
+            //Console.WriteLine("Area de Trapecio");
+            //Console.Write("Favor ingrese la base menor (a): ");
+            //a = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Favor ingrese la base mayor (b): ");
+            //b = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Favor ingrese la altura (h): ");
+            //h = Convert.ToInt32(Console.ReadLine());
+
+
+            //Console.Write("El AREA DEL TRAPECIO ES: " + h * ((a + b) / 2));
+            //Console.ReadKey();
+            #endregion
+            #region 5) Área de hexagono;
+
+
+            //int vL, vApotema;
+            //double vArea, vPerimetro;
+            //Console.WriteLine("Area de hexagono");
+            //Console.Write("Favor ingrese un lado del hexagono (Cm): ");
+            //vL = (int)Convert.ToDouble(Console.ReadLine());
+            //Console.Write("Favor ingrese la apotema del exagono en (Cm): ");
+            //vApotema = (int)Convert.ToDouble(Console.ReadLine());
+            ////Realizando el perimetro
+            //vPerimetro = vL * 6;
+            ////Realizando el Area
+            //vArea = (vPerimetro * vApotema);
+
+            //Console.Write("El AREA DEL hexagono ES: " + vArea /2 + "Cm^2");
+            //Console.ReadKey();
+
+            #endregion
+            #region 6)Área de circulo;
+            //int vRadio;
+            //Double vCalculo;
+            //const Double vPi = Math.PI;
+
+
+            //Console.WriteLine("Favor escribir el Radio del circulo :");
+            //vRadio = Int32.Parse(Console.ReadLine());
+
+            //vCalculo = vPi * Math.Pow(vRadio, 2);
+
+
+            //Console.Write("El AREA DEL CIRCULO ES:  " + vCalculo + "  Cm");
+            //Console.ReadKey();
+
+            #endregion
+            #region 7)Área de elipse;
+
+            //Double vArea ,vEjeA,vEjeB,vPerimetro;
+            //const Double vPi = Math.PI;
+
+            //Console.WriteLine("Area de la elipse");
+            //Console.WriteLine("Favor escribir el Eje menor:");
+            //vEjeA = Int32.Parse(Console.ReadLine());
+            //Console.WriteLine("Favor escribir el Eje mayor :");
+            //vEjeB = Int32.Parse(Console.ReadLine());
+
+            //vPerimetro = vPi*(vEjeA+vEjeB);
+            //vArea = vPi * vEjeA * vEjeB;
+
+
+
+
+            //Console.Write("El AREA DE LA ELIPSE ES:  " + vArea + "  Cm");
+            //Console.ReadKey();
+
+
+
+
+            #endregion
+            #region 8) Hipotenusa utilizando el teorema de Pitagoras.
+            //Double va, vb,vhipotenusa;
+            //Console.WriteLine("Bienvenido a la calculadora de La Hipotenusa utilizando el teorema de Pitagoras");
+            //Console.WriteLine("Favor escribir el cateto (a)");
+            //va = Int32.Parse(Console.ReadLine());
+            //Console.WriteLine("Favor escribir el cateto (b)");
+            //vb = Int32.Parse(Console.ReadLine());
+
+            //vhipotenusa = Math.Sqrt(Math.Pow(va, 2) + Math.Pow(vb, 2));
+            //Console.Write(" La Hipotenusa utilizando el teorema de Pitagoras ES:  " + vhipotenusa + "  Cm");
+            //Console.ReadKey();
+
+            #endregion
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Desarrollar programas (operaciones matemáticas)/int32.cs
@@ -0,0 +1,6 @@
+﻿namespace Desarrollar_programas__operaciones_matemáticas_
+{
+    internal class int32
+    {
+    }
+}
